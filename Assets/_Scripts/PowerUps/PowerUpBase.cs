edd8afc1f71c3b190ef9ab5e9bf05723ab77c791
Gustavo Hitzschky <gust.campos@gmail.com>
2022-08-29 12:49:49 -0300

    adding power up base

---
+++ Assets/_Scripts/PowerUps/PowerUpBase.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBase : ItemCollectableBase {
+
+    public float duration;
+
+    protected override void OnCollect() {
+        base.OnCollect();
+        StartPowerUp();
+    }
+
+    protected virtual void StartPowerUp() {
+        Invoke(nameof(EndPowerUp), duration);
+    }
+
+    protected virtual void EndPowerUp() {
+
+    }
+  
+}
